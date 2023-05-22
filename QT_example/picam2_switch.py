316e1b4317d39a8ae383912b3978a22ad1bb0a96
nyk.yeon <nyk.yeon@gmail.com>
2023-05-22 06:55:48 +0100

    QT 전체화면 예제 추가

---
+++ QT_example/picam2_switch.py
@@ -0,0 +1,78 @@
+# 엘레파츠 pi-cam kit 
+# picam_switch.py
+# QT app 전체화면 + GPIO 스위치 촬영 예제
+# 본 예제는 app_capture2.py 예제 기반으로 제작(수정)되었습니다
+# 원본 : https://github.com/raspberrypi/picamera2/blob/main/apps/app_capture2.py
+
+from PyQt5 import QtCore
+from PyQt5.QtWidgets import (QApplication, QHBoxLayout, QPushButton, QVBoxLayout, QWidget)
+
+from picamera2 import Picamera2
+from picamera2.previews.qt import QGlPicamera2
+from datetime import datetime
+import RPi.GPIO as GPIO
+
+#------ 해상도 설정 -------
+# LCD 해상도, [WaveShare] 4.3inch DSI LCD : 800x480
+window_width = 800
+window_height = 480
+
+# 미리보기용 (lores stream)해상도 설정, 2304x1296 권장 / 최대 해상도 설정 시 버퍼 용량 부족 문제 발생
+# LCD 해상도(main stream)보다 커야 하며, 사진 해상도와 비율이 다른 경우 촬영 사진과 다른 이미지가 표시될 수 있습니다.
+# 임의의 값 설정 시 카메라가 지원하는 해상도로 변경됩니다.
+Preview_width = 2304
+Preview_height = 1296
+
+# 촬영 사진 해상도 4608*2592 (Raspberry Pi Camera Module 3)
+cam_width = 4608
+cam_height = 2592
+#--------------------
+
+# GPIO 설정
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(21, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+# 사진 저장 경로
+save_dir = "/home/pi/Pictures/"
+
+picam2 = Picamera2()
+
+# preview 설정
+preview_config = picam2.create_preview_configuration(main={"size": (Preview_width, Preview_height)},lores={"size": (window_width, window_height)}, display="lores")
+picam2.configure(preview_config)
+
+# capture 설정
+capture_config = picam2.create_still_configuration(main={"size": (cam_width, cam_height)})
+
+
+app = QApplication([])
+
+# 스위치  촬영 함수 
+def button_clicked(channel):
+
+    now = datetime.now()
+    timeStr = now.strftime("%Y%m%d-%H%M%S")
+    picam2.switch_mode_and_capture_file(capture_config, save_dir+"picam_"+timeStr+".jpg")
+
+
+# keep_ar=True : 화면 비율 고정, keep_ar=False : 채우기
+qpicamera2 = QGlPicamera2(picam2, width=window_width, height=window_height, keep_ar=True)
+
+window = QWidget()
+
+# 함수 호출 이벤트 설정 / bouncetime: 연속 호출 제한(1~1.5초 권장, 빠르게 연속으로 누를 시 preview 화면 중단 문제 발생)
+GPIO.add_event_detect(21, GPIO.FALLING, callback=button_clicked, bouncetime=1500)        # GPIO 스위치 이벤트
+
+# 위젯 설정, 배치
+layout_h = QHBoxLayout()
+
+layout_h.addWidget(qpicamera2)
+window.setWindowTitle("Qt Picamera2 App")
+window.showFullScreen()
+window.setLayout(layout_h)
+
+# 시작
+picam2.start()
+window.show()
+app.exec()
+
--- README.md
+++ README.md
@@ -3,10 +3,12 @@
 [라즈베리파이](https://www.eleparts.co.kr/promotion/cate_event?event_seq=1622#1622_1), [PI 카메라(Camera Module 3 권장)](https://www.eleparts.co.kr/promotion/cate_event?event_seq=1622#1622_7)와 [DSI LCD](https://www.eleparts.co.kr/EPXY3WAF)를 이용하여 카메라를 제작하는 예제입니다.  
 
 [PIR 센서](https://www.eleparts.co.kr/EPXF9YKT)와 연동해 물체의 움직임이 감지되면 자동으로 촬영하는 동작도 수행 가능합니다.  
-
+  
+  
 ## 본품 구성 키트  
 [제작중입니다](https://www.eleparts.co.kr/promotion/cate_event?event_seq=1622)  
-
+  
+  
 ## 예제 다운로드&실행  
 
 라즈베리파이에서 터미널 창을 열고 아래와 같이 입력해 주면 됩니다.  
@@ -20,7 +22,8 @@ cd picam/example
 
 sudo python3 "예제 파일명(숫자 입력 후 tab키 누르면 자동완성됩니다)"
 ```  
-
+  
+  
 ## 하드웨어 연결하기  
   
 예제 실행에 앞서 CSI(CAMERA) 포트에는  [Raspberry PI 카메라)](https://www.eleparts.co.kr/promotion/cate_event?event_seq=1622#1622_7)를, DSI(DISPLAY) 포트에는 [DSI LCD](https://www.eleparts.co.kr/EPXY3WAF)를 연결 해 줍니다.  
@@ -32,8 +35,6 @@ CSI/DSI 포트를 사용하는 카메라 및 LCD는 반드시 라즈베리파이
 ![pi_cam_schematic](./schematic/pi_cam_schematic.png)  
   
   
-
-
 ## 권장 추가 설정  
 
 LCD에 화면 출력 중 절전모드 진입 시 프레임이 매우 느려집니다.  
@@ -44,17 +45,16 @@ LCD에 화면 출력 중 절전모드 진입 시 프레임이 매우 느려집�
 
 방향키로 쭉 내려 [Seat:*] 항목 아래에 있는 ```#xserver-command=X``` 을 찾아 '#' 을 삭제 후 아래와 같이 수정해 줍니다.
 
-```
+```bash
 xserver-command=X -s 0 -dpms
 
-# 마우스 커서를 숨기고 싶은 경우 아래와 같이 옵션 추가
+# 마우스 커서를 숨기고 싶은 경우(터치스크린 모드) 아래와 같이 옵션 추가
 xserver-command=X -s 0 -dpms -nocursor
 ```
-
-
-
+  
 저장 후 종료(CTRL+O, ENTER, CTRL+X)해 주시면 됩니다.
-
+  
+  
 ## 카메라 예제 시작프로그램 등록(GUI 지원)  
 
 GUI 예제는 부팅이 종료된 후 별도로 실행해 주어야 합니다.  
@@ -71,21 +71,23 @@ sudo nano /etc/xdg/lxsession/LXDE-pi/autostart
 ```bash
 lxterminal -e python3 "자동 실행할 예제 파일(경로 포함)"
 
-# 예시, 9번 예제 자동실행
-lxterminal -e python3 /home/pi/picam/example/9.LCD_cam.py
+# 예시, ~/picam/QT_example 경로의 picam2_switch.py 예제 자동실행
+lxterminal -e python3 ./picam/QT_example/picam2_switch.py
+# 또는
+lxterminal -e python3 /home/pi/picam/QT_example/picam2_switch.py    # user name 'pi'
 ```
 
-예제 파일의 절대 경로는 예제 디렉토리에서 ```pwd```명령어로 확인 가능합니다.  
+예제 파일의 전체 경로 확인 방법은 예제 디렉토리로 이동해 ```pwd```명령어를 입력해 확인 가능합니다.  
 
 
 ## 참조  
-
+  
 참조 라이브러리  
-
+  
 - picamera2 라이브러리 Github 페이지  
 https://github.com/raspberrypi/picamera2  
 라이브러리 링크의 examples 경로에 다양한 카메라 라이브러리 사용 예제가 있습니다.  
-
+  
 
 라이브러리 설명 문서  
 - picamera2 메뉴얼 문서  
